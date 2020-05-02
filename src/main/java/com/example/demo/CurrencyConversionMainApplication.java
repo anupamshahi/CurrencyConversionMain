935d71c22d6f6e2e9a30974cb471ee9d38416fdf
w2cluster121@iiht.tech <w2cluster121@iiht.tech>
2020-05-02 15:53:46 +0530

    changes done

--- src/main/java/com/example/demo/CurrencyConversionMainApplication.java
+++ src/main/java/com/example/demo/CurrencyConversionMainApplication.java
@@ -5,11 +5,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 @EnableEurekaClient
 @SpringBootApplication
 @RestController
@@ -22,6 +32,7 @@ public class CurrencyConversionMainApplication {
 	private CurrencyFeignClient currencyClient;
 	
 	@RequestMapping("/convertCurrency")
+	@HystrixCommand
 	public double home(String countryCode,Double amount) {
 		logger.info("into main class "+ countryCode + " "+amount);
 		double value = currencyClient.clientResponse(countryCode, amount);
--- src/main/java/com/example/demo/CurrencyFeignClient.java
+++ src/main/java/com/example/demo/CurrencyFeignClient.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value="CurrencyMaster")
+@RibbonClient(name="CurrencyMaster")
 public interface CurrencyFeignClient {
 	
 	@GetMapping("/convertCurrency")
